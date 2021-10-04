506ec256d523f171a4e5d44beda2f8a62de9abf2
Dhairy Tripathi <hexdecimal16@gmail.com>
2021-10-04 22:22:13 +0530

    log key listener output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import * as JZZ from 'jzz';
+import * as SYNTH from 'jzz-synth-tiny';
+import * as INPUT from 'jzz-input-kbd';
+// var JZZ = require('jzz');
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'piano-trainer';
+  inputs = [];
+  outputs = [];
+  selectedInput = null;
+  selectedOutput = null;
+
+  constructor() {
+    this.onInOutChange();
+  }
+
+  onChangeInput(event) {
+    let name = event.target.value;
+    console.log('onChangeInput', this.selectedInput);
+    JZZ().openMidiIn(name).connect((msg) => {this.initListener(msg)})
+  }
+
+  onInOutChange() {
+    JZZ().and(() => {
+      console.log('JZZ ready', JZZ.info());
+      this.inputs = []
+      this.outputs = []
+      this.inputs.push({'name': 'HTML Piano'});
+      this.outputs.push({'name': 'Not Available'});
+      this.inputs.push(...JZZ.info().inputs);
+      this.outputs.push(...JZZ.info().outputs);
+    });
+  }
+
+  initListener(msg) {
+    console.log('initListener', msg);
+  }
+
 }
